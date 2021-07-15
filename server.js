2e390af15a1739c4ebd24e9de5e54cc1ae8edc52
netharuM <netharu.m@gmail.com>
2021-07-15 18:47:33 +0530

    host fix

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ require("./models/database.conifg");
 const express = require("express");
 const userController = require("./Controller/userControl");
 const sellerController = require("./Controller/sellerControl");
-const {authRouter} = require("./Validation/Authorization");
+const { authRouter } = require("./Validation/Authorization");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", authRouter);
-app.use("/api/user",userController);
+app.use("/api/user", userController);
 app.use("/api/seller", sellerController);
 
 const port = process.env.PORT || 9090;
+const host = process.env.HOST || 'ec2-18-220-6-66.us-east-2.compute.amazonaws.com'
 
-app.listen(9090, () => {
-    console.log(`Server listening on port:${port}`);
+app.listen(port, host, () => {
+    console.log(`server started in http://${host}:${port}/api`);
 });
